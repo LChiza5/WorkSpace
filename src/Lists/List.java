f982a3274a4ca5685ab42a188a09f0042a25f4e9
PauAlvarez <pamolinaal@est.utn.ac.cr>
2025-06-27 13:47:33 -0600

    lists
    se el paquete lists con su clase list

---
+++ src/Lists/List.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Lists;
+
+/**
+ *
+ * @author HP I3
+ */
+public interface List <t>  {
+    public boolean add(T t);
+}
